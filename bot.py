47e35b89e6cff50df7876d7781a67a3ce1f80ec1
Quiet7821 <125946697+Quiet7821@users.noreply.github.com>
2023-07-31 13:04:57 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,5 +23,10 @@ async def ping (ctx):
 @client.command ()
 async def quit (ctx): 
 	await ctx.send('No, you first!')
+
+@client.tree.command(name="ping", description="Shows the bot's latency in ms")
+async def ping1 (interaction: discord.Interaction): 
+	bot_latency = round (client.latency * 1000)
+	await interaction.response.send_message (f"Pong! {bot_latency} ms")
   
 client.run (TOKEN)
